5d5b0c4861f35c00f26abd067d814e6597664714
Joseph Urschler <jmuwf4@mail.missouri.edu>
2020-07-14 15:42:27 -0700

    linked list class in progress

--- dsa-arrays-linked-lists/linked-list.js
+++ dsa-arrays-linked-lists/linked-list.js
@@ -22,17 +22,55 @@ class LinkedList {
 
   push(val) {
 
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  
+    this.length += 1;
+
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
 
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    
+    this.length += 1;
+
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+
+    let temp;
+
+    if (this.length === 0) {
+      return "Invalid"
+    } else if (this.length === 1) {
+      temp = this.head;
+      this.head = null;
+      this.tail = this.head;
+      this.length -= 1;
+      return temp;
+    } else {
+      
+    }
 
   }
 
