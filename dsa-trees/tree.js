83e7d849ff937620cb7a0e9c0a9f63699ee8acbf
Joseph Urschler <jmuwf4@mail.missouri.edu>
2020-07-24 19:59:14 -0700

    a couple methods on the tree class

--- dsa-trees/tree.js
+++ dsa-trees/tree.js
@@ -15,7 +15,20 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+
+    if (this.root === null) return 0;
+
+    let sum = this.root.val;
+
+    function _sumValues(cur) {
+
+      for (let c of cur.children) {
+        if (c.val !== null) sum += c.val;
+        if (c.children.length > 0) return _sumValues(c);
+      }
+    }
+    _sumValues(this.root);
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
