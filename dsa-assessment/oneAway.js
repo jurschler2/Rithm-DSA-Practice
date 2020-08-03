72067060267dcb2795625a9bed0113703b3dad4e
Joseph Urschler <jmuwf4@mail.missouri.edu>
2020-08-02 20:14:29 -0700

    WIP assessment challenges

--- dsa-assessment/oneAway.js
+++ dsa-assessment/oneAway.js
@@ -4,7 +4,23 @@
 
  function oneAway(s1, s2) {
 
-  
+  let l1 = s1.length;
+  let l2 = s2.length;
+
+  let diffs = 0;
+  let i = 0;
+
+
+  // Return edge cases immediately
+  if (l1 !== l2 + 1 || l1 !== l2 - 1) return false;
+
+  if (l1 === l2) {
+    for (i = 0; i < l1; i++) {
+      if (s1[i] !== s2[i]) diffs++
+    }
+    if (diffs > 1) return false;
+  }
+
 
 
  }
