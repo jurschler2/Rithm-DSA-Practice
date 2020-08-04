8cae07e6511188bb951160959ab5bd26c2c836ab
Joseph Urschler <jmuwf4@mail.missouri.edu>
2020-08-04 16:21:46 -0700

    WIP linkedlist challenge

---
+++ dsa-assessment/removeEveryOther.js
@@ -0,0 +1,19 @@
+/**
+ * Function that accepts a linked list, removes every other node of the linked list, and returns the modified linked list
+ */
+
+
+import {LinkedList, Node} from "../dsa-arrays-linked-lists/linked-list.js"
+
+function removeEveryOther(ll) {
+
+  if (!ll.head || ll.length < 2) return ll;
+
+  
+
+
+
+
+}
+
+module.exports = removeEveryOther;
