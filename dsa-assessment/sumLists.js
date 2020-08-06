5d20b9b7755fef17a4e3206e6659ea4f39019dbf
Joseph Urschler <jmuwf4@mail.missouri.edu>
2020-08-05 17:06:05 -0700

    sumLists challenge completed, leveraged earlier written linkedlist code, need to write tests

---
+++ dsa-assessment/sumLists.js
@@ -0,0 +1,35 @@
+/**
+ * Function that accepts two linked lists which represent two numbers. Each
+ */
+
+
+const LinkedList = require("../dsa-arrays-linked-lists/linked-list.js");
+
+function sumLists(l1, l2) {
+
+  if (!(l1 instanceof LinkedList) || !(l2 instanceof LinkedList)) throw new Error("Please submit a linked list.");
+
+  if (!l1.head && !l2.head) return new LinkedList();
+
+  if (!l1.head) {
+    return l2.print();
+  } else if (!l2.head) {
+    return l1.print();
+  }
+
+  let num1 = parseInt(l1.print().join(''))
+  let num2 = parseInt(l2.print().join(''))
+
+  let num3 = num1 + num2;
+
+  let num3Arr = `${num3}`.split('');
+
+  num3Arr = num3Arr.map(a => parseInt(a));
+
+  let answer = new LinkedList(num3Arr);
+
+  return answer.print();
+
+}
+
+module.exports = sumLists;
